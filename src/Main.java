f4b57e115938231b5aa363549a233a117f1eb8fa
nirmalsharma <nirmal.sharma3500@gmail.com>
2022-09-18 18:37:51 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //Get the order of type order list using Order Management microservice
+        //getOrders();
+        List<Order> orderList= new ArrayList<>();
+        //Iterate all the order and perform below operations for each order
+        //1 get wrapped Package from Packaging management microservice by providing order details including address
+        //2 Send the package to delivery management service
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/Order.java
@@ -0,0 +1,9 @@
+public class Order {
+    String address;
+    String typeOfDuck;
+
+    public Order(String address, String typeOfDuck) {
+        this.address = address;
+        this.typeOfDuck = typeOfDuck;
+    }
+}
